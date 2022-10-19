25df8677bda2ce8b9441ad3bdadcd2853a4cb742
Movsar Bekaev <bekaev.movsar@gmail.com>
2022-10-19 22:49:22 +0300

    works well

--- Data/ServiceCollectionExtensions.cs
+++ Data/ServiceCollectionExtensions.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Data {
     public static class ServiceCollectionExtensions {
         public static void RegisterServices(this IServiceCollection services) {
-            var cs = "Server=172.17.0.3,1433;User ID=SA;Password=<YourStrong@Passw0rd>;Database=Tasks;Connect Timeout=30;Encrypt=False;";
+            var cs = "Server=sql_server2022;User ID=SA;Password=<YourStrong@Passw0rd>;Database=Tasks;Connect Timeout=30;Encrypt=False;";
             services.AddDbContext<DataContext>(options => options.UseSqlServer(cs));
             services.AddScoped<TaskRepository>();
             services.AddScoped<Storage>();
